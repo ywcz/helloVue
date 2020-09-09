906dfd9c8144f3ad0ff636efd15065ee7fa167d0
watermelon <ywczxj@qq.com>
2020-09-09 22:33:40 +0800

    update

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,70 +1,12 @@
 <template>
-  <div>
-
-    <b-row>
-      <b-col
-        md="6"
-        offset-md="3"
-      >
-        <b-card>
-          <b-form>
-            <b-form-group
-              id="input-group-1"
-              label="UserName:"
-              label-for="input-1"
-            >
-              <b-form-input
-                id="input-1"
-                v-model="user.name"
-                type="text"
-                required
-                placeholder="Enter userName"
-              ></b-form-input>
-            </b-form-group>
-            <b-form-group
-              id="input-group-2"
-              label="PhoneNumber:"
-              label-for="input-2"
-            >
-              <b-form-input
-                id="input-2"
-                v-model="user.phoneNumber"
-                type="text"
-                required
-                placeholder="Enter phoneNumber"
-              ></b-form-input>
-            </b-form-group>
-            <b-form-group
-              id="input-group-3"
-              label="Password:"
-              label-for="input-3"
-            >
-              <b-form-input
-                id="input-3"
-                v-model="user.password"
-                type="password"
-                required
-                placeholder="Enter password"
-              ></b-form-input>
-            </b-form-group>
-          </b-form>
-        </b-card>
-      </b-col>
-    </b-row>
-
-  </div>
+  <div></div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      user: {
-        name: '',
-        phoneNumber: '',
-        password: '',
-      },
-    };
-  },
+
 };
 </script>
+
+<style>
+</style>
